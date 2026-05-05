ead79df73a3600b37f81f46c86c0d378efeb9368
Nithya-collab <141161666+Nithya-collab@users.noreply.github.com>
2026-05-05 07:05:08 +0530

    improve /cron-trigger fucnatiolity

--- routes/index.js
+++ routes/index.js
@@ -71,16 +71,26 @@ router.get('/test-email', async (req, res) => {
 
 
 // Public route for cron-job.org to trigger the reminders
-router.get('/cron-trigger', async (req, res) => {
-    try {
-        // You can manually trigger your cron logic here if needed
-        const { checkAndSendReminders } = require('../services/cronService');
-        await checkAndSendReminders();
-        res.status(200).send("Cron triggered successfully");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Cron trigger failed");
-    }
+// router.get('/cron-trigger', async (req, res) => {
+//     try {
+//         // You can manually trigger your cron logic here if needed
+//         const { checkAndSendReminders } = require('../services/cronService');
+//         await checkAndSendReminders();
+//         res.status(200).send("Cron triggered successfully");
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send("Cron trigger failed");
+//     }
+// });
+
+router.get('/cron-trigger', (req, res) => {
+    res.status(200).send("OK");
+    const { checkAndSendReminders } = require('../services/cronService');
+
+    checkAndSendReminders()
+        .then(() => console.log("Cron job done"))
+        .catch(err => console.error("Cron error:", err));
 });
+
 
 module.exports = router;
